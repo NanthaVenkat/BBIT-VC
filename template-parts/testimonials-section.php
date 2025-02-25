2ff6509997d6421b9160c63235a0a9dfb842eb45
NnthaKumar <vnanthakumar00@gmail.com>
2025-02-25 13:14:35 +0530

    individual doctor page completed two

---
+++ template-parts/testimonials-section.php
@@ -0,0 +1,114 @@
+<section class="testimonial py-4 py-md-5">
+    <div class="container">
+        <div class="text-center mb-4 mb-md-5">
+            <p class="titles-title text-sm mb-2">3940+ Happy Customers</p>
+            <h3 class="main-title">Don’t just take our words</h3>
+        </div>
+
+        <!-- testimonial slider -->
+        <div class="swiper-container testimonial-slider position-relative pb-4 pb-sm-0 overflow-hidden">
+
+            <!-- Navigation Buttons -->
+            <div class="swiper-button-next two d-none d-sm-flex"></div>
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="testimonial-wrpr align-items-center d-flex">
+                        <div class="profile rounded-4 overflow-hidden d-flex align-items-center justify-content-center me-0 me-sm-4 mb-4 mb-sm-0">
+                            <img src="./Images/review.webp" alt="Person">
+                        </div>
+                        <div class="coments">
+                            <div class="testimonial-cnt text-center text-md-start">
+                                <div class="str-ratings mb-3 mb-sm-4">
+                                    <div class="start-wrpper align-items-center d-flex justify-content-center justify-content-sm-start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                    </div>
+                                </div>
+                                <p class="ff-helvetica text-sm">I had a very busy schedule which made visiting hospitals a difficult task for me. That is when I came upon this webpage from Sri Ramakrishna Hospital.</p>
+                                <h5 class="ff-degular text-md">Suganya</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-wrpr align-items-center d-flex">
+                        <div class="profile rounded-4 overflow-hidden d-flex align-items-center justify-content-center me-0 me-sm-4 mb-4 mb-sm-0">
+                            <img src="./Images/review(2).webp" alt="Person">
+                        </div>
+                        <div class="coments">
+                            <div class="testimonial-cnt text-center text-md-start">
+                                <div class="str-ratings mb-3 mb-sm-4">
+                                    <div class="start-wrpper align-items-center d-flex justify-content-center justify-content-sm-start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                    </div>
+                                </div>
+                                <p class="ff-helvetica text-sms">I had a very busy schedule which made visiting hospitals a difficult task for me. That is when I came upon this webpage from Sri Ramakrishna Hospital.</p>
+                                <h5 class="ff-degular text-md">Suganya</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-wrpr align-items-center d-flex">
+                        <div class="profile rounded-4 overflow-hidden d-flex align-items-center justify-content-center me-0 me-sm-4 mb-4 mb-sm-0">
+                            <img src="./Images/review.webp" alt="Person">
+                        </div>
+                        <div class="coments">
+                            <div class="testimonial-cnt text-center text-md-start">
+                                <div class="str-ratings mb-3 mb-sm-4">
+                                    <div class="start-wrpper align-items-center d-flex justify-content-center justify-content-sm-start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                    </div>
+                                </div>
+                                <p class="ff-helvetica text-sm">I had a very busy schedule which made visiting hospitals a difficult task for me. That is when I came upon this webpage from Sri Ramakrishna Hospital.</p>
+                                <h5 class="ff-degular text-md">Suganya</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="testimonial-wrpr align-items-center d-flex">
+                        <div class="profile rounded-4 overflow-hidden d-flex align-items-center justify-content-center me-0 me-sm-4 mb-4 mb-sm-0">
+                            <img src="./Images/review(2).webp" alt="Person">
+                        </div>
+                        <div class="coments">
+                            <div class="testimonial-cnt text-center text-md-start">
+                                <div class="str-ratings mb-3 mb-sm-4">
+                                    <div class="start-wrpper align-items-center d-flex justify-content-center justify-content-sm-start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                        <img src="./Images/RatingStar.svg" alt="start">
+                                    </div>
+                                </div>
+                                <p class="ff-helvetica text-sms">I had a very busy schedule which made visiting hospitals a difficult task for me. That is when I came upon this webpage from Sri Ramakrishna Hospital.</p>
+                                <h5 class="ff-degular text-md">Suganya</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pagination -->
+            <div class="swiper-pagination justify-content-center d-flex d-sm-none"></div>
+
+            <!-- Navigation Buttons -->
+            <div class="swiper-button-prev two d-none d-sm-flex"></div>
+        </div>
+    </div>
+</section>
